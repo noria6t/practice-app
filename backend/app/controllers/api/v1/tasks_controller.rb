d04815829d842a6cb2699fa453ecc0791fee2788
noria6t <takahatanoria@gmail.com>
2022-05-22 21:16:41 +0900

    initial commit

---
+++ backend/app/controllers/api/v1/tasks_controller.rb
@@ -0,0 +1,16 @@
+module Api
+    module V1
+      class TasksController < ApplicationController
+        def index
+          # byebug
+          if Task.count.zero?
+            ['1st task', '2nd task', '3rd task'].each do |title|
+              Task.create!(title: title)
+            end
+          end
+
+          render json: { tasks: Task.all.to_json }
+        end
+      end
+    end
+  end
---
+++ frontend/components/Tutorial.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">
+        Zenn-app
+      </h1>
+      <h2 class="subtitle">
+        {{ subTitle }}
+      </h2>
+      <button @click="getSomething">
+        タスク取得
+      </button>
+      <ul v-for=" task in tasks" :key="task.id">
+        <li style="text-align: left;">{{ task.title }}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      subTitle: 'Zenn is good service!!',
+      tasks: []
+    }
+  },
+  methods: {
+    async getSomething() {
+      const response = await this.$axios.$get('http://localhost:5000/api/v1/tasks')
+      console.log(response)
+      console.log(response.tasks)
+      this.tasks = JSON.parse(response.tasks)
+      console.log(this.tasks)
+    }
+  //   async getData() {
+  // const res = await this.$axios('http://localhost:8080/').catch(err => {
+  //   return err.response
+  // })
+// }
+  },
+}
+</script>
